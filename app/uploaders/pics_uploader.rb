6ef1f374e2cda0b3b5805c758f288f2ddb5e082c
Rami <sairam1284@yahoo.com>
2017-05-15 11:09:08 -0400

    Remove JS test

--- app/uploaders/pics_uploader.rb
+++ app/uploaders/pics_uploader.rb
@@ -1,11 +1,15 @@
 class PicsUploader < CarrierWave::Uploader::Base
-
+  if Rails.env.test?
+      storage :file
+    else
+      storage :fog
+  end
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -36,9 +40,9 @@ class PicsUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
